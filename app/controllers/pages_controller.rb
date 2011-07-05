6bcdd7eb137f006cd7a56b3a97ba5a4e819571c0
Morgan Page <me@morganpage.co.uk>
2011-07-05 17:48:32 +0100

    Space dust hscore table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,23 @@ class PagesController < ApplicationController
 
   def home
     @title = "Home"
+    @twitter = "http://twitter.com/#!/PolityAI"
+    @facebook = "http://www.facebook.com/morganjpage"
   end
 
   def games
     @title = "Games"
   end
   
+  
+  
+  def spacedust
+    @title = "Space Dust"
+    @high_scores = Highscore.find(:all, :limit => 100,
+                                  :order => 'score desc')
+    respond_to do |format|
+      format.html  
+  end  
+  
+  end
 end
